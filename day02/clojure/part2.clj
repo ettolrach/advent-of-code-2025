d806f059ac06d7778e1c42c0cdeb0a39e3f1d91a
Charlotte Ausel <ettolrach@disroot.org>
2025-12-02 18:41:44 +0000

    clojure almost correct!

--- day02/clojure/part2.clj
+++ day02/clojure/part2.clj
@@ -4,9 +4,9 @@
 (defn is-invalid [s]
   (let [mid (/ (count s) 2)]
     ;; evil, evil, evil shit! we're exploiting that a != b => hash a != hash b.
-    (map
+    (some true? (map
      #(== 1 (count (keys (group-by hash (re-seq (re-pattern (format ".{1,%d}" %)) s)))))
-     (range 1 (+ 1 mid)))))
+     (range 1 (+ 1 mid))))))
 (defn check-string [s] (if (is-invalid (str s)) s 0))
 (defn gen-range [l] (range (Long/parseLong (nth l 0)) (+ 1 (Long/parseLong (nth l 1)))))
 (def input (slurp *in*))
