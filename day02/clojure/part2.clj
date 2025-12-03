1c4c9e1b1bfa39c916033bb3d1e21d497c1b406f
Charlotte Ausel <ettolrach@disroot.org>
2025-12-03 10:43:01 +0000

    giving up on clojure

--- day02/clojure/part2.clj
+++ day02/clojure/part2.clj
@@ -1,11 +1,19 @@
 (require '[clojure.string :as str])
 
 (defn parse [input] (map #(str/split % #"-") (str/split input #",")))
+(defn is-equal [l]
+  (if
+    (empty? l)
+    false
+    (let [h (nth l 1)]
+       (every?
+        #(= h %)
+        l))))
 (defn is-invalid [s]
   (let [mid (/ (count s) 2)]
-    ;; evil, evil, evil shit! we're exploiting that a != b => hash a != hash b.
     (some true? (map
-     #(== 1 (count (keys (group-by hash (re-seq (re-pattern (format ".{1,%d}" %)) s)))))
+     #(is-equal (re-seq (re-pattern (format ".{1,%d}" %)) s))
+     ;; #(== 1 (count (keys (group-by hash (re-seq (re-pattern (format ".{1,%d}" %)) s)))))
      (range 1 (+ 1 mid))))))
 (defn check-string [s] (if (is-invalid (str s)) s 0))
 (defn gen-range [l] (range (Long/parseLong (nth l 0)) (+ 1 (Long/parseLong (nth l 1)))))
